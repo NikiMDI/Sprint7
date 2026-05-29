46b3cc9560accd269b1d68c0aa33b3adc1e05b38
Maseev.Nikita <nikita.maseev.2003@mail.ru>
2026-05-29 21:54:33 +0400

    добавил генерацию данных

--- src/test/java/testcomplect/OrderCreateTest.java
+++ src/test/java/testcomplect/OrderCreateTest.java
@@ -1,17 +1,22 @@
 package testcomplect;
 
+import com.github.javafaker.Faker;
 import io.restassured.response.Response;
 import modelclasses.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import steps.OrderSteps;
-import static org.hamcrest.Matchers.notNullValue;
+
+import java.util.Locale;
 import java.util.stream.Stream;
+
+import static org.hamcrest.Matchers.notNullValue;
 
 public class OrderCreateTest {
 
     private final OrderSteps orderSteps = new OrderSteps();
+    private final Faker faker = new Faker(new Locale("ru"));
 
     public static Stream<Arguments> colorData() {
 
@@ -28,14 +33,14 @@ public class OrderCreateTest {
     public void orderCanBeCreatedWithDifferentColors(String[] colors) {
 
         Order order = new Order(
-                "Nikita",
-                "Test",
-                "Пушкина 3",
-                1,
-                "+79999999999",
-                5,
+                faker.name().firstName(),
+                faker.name().lastName(),
+                faker.address().streetAddress(),
+                faker.number().numberBetween(1, 100),
+                faker.phoneNumber().cellPhone(),
+                faker.number().numberBetween(1, 10),
                 "2026-06-06",
-                "Test order",
+                faker.lorem().sentence(),
                 colors
         );
 
